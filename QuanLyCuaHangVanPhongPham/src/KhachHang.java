f9649f708abbfe2b62f66341117a7744e6d181c4
VanuLL <88327199+VanuLL@users.noreply.github.com>
2022-01-09 13:20:41 +0700

    Add files via upload

---
+++ QuanLyCuaHangVanPhongPham/src/KhachHang.java
@@ -0,0 +1,33 @@
+
+public class KhachHang extends Nguoi {
+	
+	public KhachHang(String id, String ten, String tuoi, String sdt) {
+		super(id, ten, tuoi, sdt);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KhachHang() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return super.getId() +";"+super.getTen()+";"+super.getTuoi() +";"+super.getSdt()+";";
+	}
+	@Override
+	public void luong() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void nhapKhachHang() {
+		super.nhapNguoi();
+	}
+	
+	public void xuatKhachHang()	{
+		super.xuatNguoi();
+	}
+	public void phuongThucThanhToan() {
+		System.out.println("Khách hàng trả bằng cách quẹt thẻ, trả bằng tiền mặt!");
+	}
+	
+}
